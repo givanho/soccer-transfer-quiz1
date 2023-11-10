993188a76af64247c1c04adf419b462dbc98cb0a
Given Fyneface <fynefaceg@gmail.com>
2023-11-10 12:35:03 +0100

    questions

---
+++ categories/transfers/questions.js
@@ -0,0 +1,44 @@
+
+const quizdata = [
+  
+    
+   
+      {
+        id: 1,
+        questionImages: [
+         require("../../assets/England/Arsenal.png"),
+         require("../../assets/England/bournemouth.png"),
+         require("../../assets/England/Brentford.png"),
+         require("../../assets/England/burnley.png"),
+          
+        ],
+        options: [ "Anelka", "Wallcot","D. Drogba","J. Okocha" ],
+        answer: "Anelka"
+      },
+      {
+        id: 2,
+        questionImages: [
+          require("../../assets/England/luton.png"),
+          require("../../assets/England/fulham.png"),
+          require("../../assets/England/Liverpool.png"),
+          require("../../assets/England/chelsea.png"),
+        ],
+        options: [ "Benzema", "Bale","D. Berbatov","J. Barton" ],
+        answer: "D. Berbatov"
+      },
+      {
+        id: 3,
+        questionImages: [
+          require("../../assets/England/WestHam.png"),
+          require("../../assets/England/Liverpool.png"),
+          require("../../assets/Spain/barcelona.png"),
+          require("../../assets/others/hebei.png"),
+        ],
+        options: ["Javier Mascherano", "Yossi Benayoun", "Alberto Moreno", "Fabio Aurelio"],
+        answer: "Javier Mascherano"
+      },
+  
+  
+]
+
+export default quizdata
